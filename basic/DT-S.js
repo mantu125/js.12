94ae9aa42c2eedd5060ad74276610d654afb0cd6
s.mantu <mantu.bay20@gmail.com>
2025-09-23 10:26:54 +0000

    DT-S.js

---
+++ basic/DT-S.js
@@ -0,0 +1,43 @@
+// datatypes-summary.js
+
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // false
+
+// const bigNumber = 1233547896556655 // not bigInt
+// const bigNumber = 1233547896556655n  // bigInt
+
+
+// Reference (Non primitive)
+
+// Array, Object, Function
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj ={
+    name: "Mantu",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");    
+}
+
+// console.log(typeof heros); // Object
+// console.log(typeof outsideTemp); // object
+// console.log(typeof scoreValue); // number
+// console.log(typeof bigNumber); // BigInt
+// console.log( typeof myFunction); // function
+// console.log(typeof Symbol); // function
+
