4469b85379d72e371ef3734802769618ec6a7a7f
s.mantu <mantu.bay20@gmail.com>
2025-09-23 09:33:35 +0000

    CO.js

--- basic/CO.js
+++ basic/CO.js
@@ -26,10 +26,62 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber); // string
-console.log(typeof stringNumber); // string
+// let stringNumber = String(someNumber)
+// console.log(stringNumber); // string
+// console.log(typeof stringNumber); // string
+
+
+// Operation 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Mantu"
+
+let str3 = str1 + str2
+// console.log(str3); // display inline
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log( (3 + 4) * 5 % 3); // 2
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(""); // --
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1=num2=num3 = 2+2
+
+let gameCounter = 100
+// ++gameCounter; // 101
+gameCounter++; // 101
+console.log(gameCounter); // 101
+
+
+
+
+
+
+
+
+
+
 
 
