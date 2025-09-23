9776437386c262e2b5b006b3d4bdaa824c3fe29a
s.mantu <mantu.bay20@gmail.com>
2025-09-23 08:53:08 +0000

    CO.js

---
+++ basic/CO.js
@@ -0,0 +1,35 @@
+// ConversionOperation.js
+
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = false
+// let score = true
+
+// console.log(typeof score); // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>; false => 0
+
+// let isLoggedIn = 1 // true
+// let isLoggedIn = "Mantu" // true
+// let isLoggedIn = "" // false
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber); // string
+console.log(typeof stringNumber); // string
+
+
