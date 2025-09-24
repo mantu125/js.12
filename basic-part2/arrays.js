c10c47179881c385a6ee40f967e0cb847a23b87c
s.mantu <mantu.bay20@gmail.com>
2025-09-24 11:21:50 +0000

    arrays.js

--- basic-part2/arrays.js
+++ basic-part2/arrays.js
@@ -36,17 +36,40 @@ const newArr = myArr.join()
 // console.log(myArr); // [ 0, 1, 2, 3, 4, 5 ]
 
 // slice, splice
-console.log("A", myArr); // A [ 0, 1, 2, 3, 4, 5 ]
+// console.log("A", myArr); // A [ 0, 1, 2, 3, 4, 5 ]
 
 const myn1 = myArr.slice(1, 3) 
-console.log(myn1); // [ 1, 2 ] not add last number
-console.log("B", myArr); // B [ 0, 1, 2, 3, 4, 5 ]
+// console.log(myn1); // [ 1, 2 ] not add last number
+// console.log("B", myArr); // B [ 0, 1, 2, 3, 4, 5 ]
 
 const myn2 = myArr.splice(1, 3)
-console.log("C", myArr); // [ 0, 4, 5 ]
-console.log(myn2); // [ 1, 2, 3 ] all
+// console.log("C", myArr); // [ 0, 4, 5 ]
+// console.log(myn2); // [ 1, 2, 3 ] all
 
 // Array
 
 const marvel_heros = ["Thor", "Ironman", "Spyderman"]
 const dc_heros = ["Batman", "Flash", "superman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros); // [ 'Thor', 'Ironman', 'Spyderman', [ 'Batman', 'Flash', 'superman' ] ] Joined
+// console.log(marvel_heros[3][1]); // flash
+ 
+// const allheros = marvel_heros.concat(dc_heros)
+// console.log(allheros); // [ 'Thor', 'Ironman', 'Spyderman', 'Batman', 'Flash', 'superman' ] joined
+
+// const all_new_heros =[...marvel_heros, ...dc_heros]
+// console.log(all_new_heros); // [ 'Thor', 'Ironman', 'Spyderman', 'Batman', 'Flash', 'superman' ] 
+
+const anotherArray = [1, 2, 3, [4, 5, 6], 7, [8, 9, [10, 11]]]
+const real_anotherArray = anotherArray.flat(Infinity)
+// console.log(real_anotherArray); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10, 11]
+
+// console.log(Array.isArray("Mantu")) // false
+// console.log(Array.from("Mantu")) // [ 'M', 'a', 'n', 't', 'u' ]
+// console.log(Array.from({name: "Mantu"})) // []
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3)); // [ 100, 200, 300 ]
