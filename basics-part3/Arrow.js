16431eebc0ecb3fe695c7a3e36d7eb144180d5c5
s.mantu <mantu.bay20@gmail.com>
2025-10-08 04:53:09 +0000

    Arrow.js

--- basics-part3/Arrow.js
+++ basics-part3/Arrow.js
@@ -10,4 +10,42 @@ const user = {
 // user.username = "sam" 
 // user.welcomeMessage() // sam, welcome to website
 
-console.log(this); // {}
+// console.log(this); // {}
+
+// function chai(){
+//     let username = "Mantu"
+//     console.log(this.username); // undefined
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "Mantu"
+//     console.log(this.username); // undefined
+// }
+// chai()
+
+// const chai = () => {
+//     let username = "Mantu"
+//     console.log(this); // {}
+//     console.log(this.username); // undefined
+// }
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3, 4)); // 7
+
+// const addTwo = (num1, num2) => num1 + num2
+// console.log(addTwo(4, 20)); // 24
+
+// const addTwo = (num1, num2) => (num1 + num2)
+// console.log(addTwo(4, 20)); // 24
+
+// const addTwo = (num1, num2) => {username: "Mantu"}
+// console.log(addTwo(4, 20)); // undefined
+// const addTwo = (num1, num2) => ({username: "Mantu"})
+// console.log(addTwo(4, 20)); // { username: 'Mantu' }
+
+// const MyArray = [2, 5, 6, 8]
+// MyArray.forEach()
